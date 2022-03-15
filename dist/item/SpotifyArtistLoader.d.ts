c40114d96018bc20569b6ef82ddcfe363d573708
ssMMiles <github@milesmoonlove.com>
2022-03-15 03:15:13 +0000

    why'd it run ok this time

--- dist/item/SpotifyArtistLoader.d.ts
+++
@@ -1,9 +0,0 @@
-import { Loader } from "../abstract/Loader";
-import { SpotifyItemType } from "../abstract/SpotifyItem";
-import { SpotifyArtist } from "./SpotifyArtist";
-import type { SpotifyManager } from "../SpotifyManager";
-export declare class SpotifyArtistLoader extends Loader {
-    itemType: SpotifyItemType;
-    matchers: RegExp[];
-    load(manager: SpotifyManager, [, id]: RegExpExecArray): Promise<SpotifyArtist>;
-}
